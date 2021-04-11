191f9a315105d0995dbedb9db13d7b4f45c74471
Nirusduddin Mateh (YaraTeam) <69489799+nirusduddinmateh@users.noreply.github.com>
2021-04-11 09:38:13 +0700

    Update UserController.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::query()->paginate();
+        $users = User::query()->get();
         return response()->json($users);
     }
 
